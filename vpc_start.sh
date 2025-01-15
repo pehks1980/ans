08fa16f210c6bd6ab5e743d755905f4ea7e00a73
Pehks <pehks1980@gmail.com>
2025-01-15 13:25:31 +0300

    added advanced hosts

--- vpc_start.sh
+++ vpc_start.sh
@@ -13,11 +13,12 @@ fi
 
 echo "copy ansible config files to server folder : ${ans_folder}."
 
-python3 gen_ans_inventory.py
+python3 gen_ans_inventory1.py
 
 ansible_ip=$(python3 get_ansible_server_ip.py)
 #python3 get_ansible_server_ip.py | xargs -I {} scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ans_conf/hosts ubuntu@{}:${ans_folder}/hosts
 scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ans_conf/hosts ubuntu@${ansible_ip}:${ans_folder}/hosts
+scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ans_conf/hosts.txt ubuntu@${ansible_ip}:${ans_folder}/hosts.txt
 #python3 get_ansible_server_ip.py | xargs -I {} scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ans_conf/ansible.cfg ubuntu@{}:${ans_folder}/ansible.cfg
 scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ans_conf/ansible.cfg ubuntu@${ansible_ip}:${ans_folder}/ansible.cfg
 ssh ubuntu@${ansible_ip} "cd ansible && ansible --version && ansible -v -i hosts all -m ping"
